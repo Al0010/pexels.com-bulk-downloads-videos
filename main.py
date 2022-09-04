b719e7741d70a451ccc732de4b9c30cf6930c372
Alex <100917872+Al0010@users.noreply.github.com>
2022-09-04 15:28:05 +0200

    Create main.py

---
+++ main.py
@@ -0,0 +1,18 @@
+import pexelsPy
+import requests
+
+PEXELS_API = 'ADDING YOUR APY KEY HERE'
+api = pexelsPy.API(PEXELS_API)
+
+pageNumbers = 1 
+resultsPage = 5 
+
+api.search_videos('Sea', page=pageNumbers, results_per_page=resultsPage)
+videos = api.get_videos()
+
+
+for data in videos:
+    url_video = 'https://www.pexels.com/video/' + str(data.id) + '/download'
+    r = requests.get(url_video)
+    with open(data.url.split('/')[-2]+'.mp4', 'wb') as outfile:
+        outfile.write(r.content)
